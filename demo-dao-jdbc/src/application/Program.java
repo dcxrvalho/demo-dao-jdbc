7661b08c7771aa4d9fb5dc30cf9339b11f674cee
dcxrvalho <densetsunoban@gmail.com>
2025-08-13 15:31:04 -0300

    department and seller entity classes

---
+++ demo-dao-jdbc/src/application/Program.java
@@ -0,0 +1,17 @@
+package application;
+
+import java.util.Date;
+
+import model.entities.Department;
+import model.entities.Seller;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Department dept = new Department(1, "Books");
+		Seller seller = new Seller(20, "Izabella", "izabella@gmail.com", new Date(), 10000.00, dept);
+		
+		System.out.print(seller);
+	}
+}
